f68c6b0bd51153b9d983fdbd027851c7d7e15cb9
Iraku Harry <iraqooh@gmail.com>
2024-09-18 08:39:59 +0300

    CORS2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const cors_options = {
   origin: '*', // Allows all origins. For production, replace with specific domains.
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify the HTTP methods to allow
-  allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+  allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'], // Specify allowed headers
 };
 app.use(cors(cors_options));
 
